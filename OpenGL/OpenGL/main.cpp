820d0e4e129cd7da1fbf4c82b2a29755b7ac77b5
Jack Anderson <jack_120@hotmail.co.uk>
2016-05-06 15:48:15 +0100

    display clear color

--- OpenGL/OpenGL/main.cpp
+++ OpenGL/OpenGL/main.cpp
@@ -1,7 +1,17 @@
 #include "display.h"
+#include <GL/glew.h>
 
 int main(int argc, char *argv[])
 {
 	Display display(800, 600, "Hello World!");
+	
+	while (!display.IsClosed())
+	{
+		glClearColor(0.0f, 0.15f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		display.Update();
+	}
+	
 	return 0;
 }
