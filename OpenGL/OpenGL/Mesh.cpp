17d45cf86b0b9c1be272846c3d80c3df99bdad26
Jack Anderson <jack_120@hotmail.co.uk>
2016-05-06 20:08:35 +0100

    Created mesh class

---
+++ OpenGL/OpenGL/Mesh.cpp
@@ -0,0 +1,14 @@
+#include "Mesh.h"
+
+Mesh::Mesh()
+{
+}
+
+void Mesh::Draw()
+{
+
+}
+
+Mesh::~Mesh()
+{
+}
---
+++ OpenGL/OpenGL/Mesh.h
@@ -0,0 +1,17 @@
+#ifndef MESH_H
+#define MESH_H
+
+class Mesh
+{
+public:
+	Mesh();
+
+	void Draw();
+
+	virtual ~Mesh();
+protected:
+private:
+};
+
+#endif
+
