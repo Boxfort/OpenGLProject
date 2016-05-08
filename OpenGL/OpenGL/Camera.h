7f710ff1b5dc5dae86f652b6f5d35ee25d92b7d3
Jack Anderson <jack_120@hotmail.co.uk>
2016-05-08 15:54:25 +0100

    added camera class

---
+++ OpenGL/OpenGL/Camera.h
@@ -0,0 +1,17 @@
+#ifndef CAMERA_H_INCLUDED
+#define CAMERA_H_INCLUDED
+
+#include <glm/glm.hpp>
+#include <glm/gtx/transform.hpp>
+
+class Camera
+{
+public:
+	Camera(const glm::vec3&);
+	~Camera();
+protected:
+private:
+};
+
+#endif
+
